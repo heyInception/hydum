3c216ed3d561111ea02378722cbc27161615a6d3
Anton Schastlivtsev <antonschastlivc@gmail.com>
2023-01-19 16:35:40 +0400

    Create JS file

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,1 +1,38 @@
-console.log('components');
+try {
+    const button = document.querySelector('[data-js="spoilerToggle"]');
+
+    const spoilerToggle = e => {
+        console.log(e.target.dataset.targetid)
+        const spoiler = document.querySelector(`[data-js="spoiler"][data-targetId="${e.target.dataset.targetid}"]`);
+
+        const spoilerContentHeight = spoiler.children[0].clientHeight;
+
+        spoiler.style.maxHeight = `${spoilerContentHeight}px`;
+
+        spoiler.classList.toggle('open');
+        clearTimeout(animationTimeout);
+
+        if (spoiler.classList.contains('open')) {
+            animationTimeout = setTimeout(() => {
+                spoiler.style.maxHeight = 'none';
+            }, 1000);
+        } else {
+            animationTimeout = setTimeout(() => {
+                spoiler.style.maxHeight = '290px';
+            }, 0);
+        }
+    }
+    let animationTimeout = null;
+
+    button.addEventListener('click', spoilerToggle);
+} catch (error) {
+    console.log('Нет блока сео')
+}
+
+
+const search = document.querySelectorAll('.header__search svg')
+search.forEach(e => {
+    e.addEventListener('click', function() {
+        document.querySelector('.search_desk').classList.toggle('search_open')
+    })
+})
--- src/js/_functions.js
+++ src/js/_functions.js
@@ -21,7 +21,7 @@ console.log(mobileCheck())
 // import './functions/fix-fullheight';
 
 // Реализация бургер-меню
-// import { burger } from './functions/burger';
+import { burger } from './functions/burger';
 
 // Реализация остановки скролла (не забудьте вызвать функцию)
 // import { disableScroll } from './functions/disable-scroll';
@@ -30,12 +30,12 @@ console.log(mobileCheck())
 // import { enableScroll } from './functions/disable-scroll';
 
 // Реализация модального окна
-// import GraphModal from 'graph-modal';
-// const modal = new GraphModal();
+import GraphModal from 'graph-modal';
+const modal = new GraphModal();
 
 // Реализация табов
-// import GraphTabs from 'graph-tabs';
-// const tabs = new GraphTabs('tab');
+import GraphTabs from 'graph-tabs';
+const tabsProduct = new GraphTabs('productTabs');
 
 // Получение высоты шапки сайта (не забудьте вызвать функцию)
 // import { getHeaderHeight } from './functions/header-height';
@@ -50,11 +50,161 @@ console.log(mobileCheck())
 // });
 
 // Подключение свайпера
-// import Swiper, { Navigation, Pagination } from 'swiper';
-// Swiper.use([Navigation, Pagination]);
-// const swiper = new Swiper(el, {
-//   slidesPerView: 'auto',
-// });
+import Swiper, { Navigation, Pagination, Scrollbar, Thumbs, FreeMode } from 'swiper';
+Swiper.use([Navigation, Pagination, Scrollbar, Thumbs, FreeMode]);
+/*const swiper = new Swiper('.gallery__slider', {
+  slidesPerView: '4',
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  spaceBetween: 22
+});*/
+
+const swiperOptions = {
+    slidesPerView: '4',
+    spaceBetween: 22,
+    // custom pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function(i, className) {
+            return `
+   <button class="${className}">
+  <svg class="progress" width="41" height="41"><circle class="circle-origin" r="20" cx="20.5" cy="20.5"></circle></svg><span>${i + 1
+        }</span>
+</button>
+      `;
+        }
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            pagination: {
+                el: ".swiper-pagination",
+            },
+        },
+        // when window width is >= 480px
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 22
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 22
+        },
+        // when window width is >= 640px
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 22
+        }
+    }
+};
+
+const swiperGallery = new Swiper(".gallery__slider", swiperOptions);
+
+
+// breakpoint where swiper will be destroyed
+// and switches to a dual-column layout
+const breakpoint = window.matchMedia('(min-width:768px)');
+// keep track of swiper instances to destroy later
+let swiperHero;
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+const breakpointChecker = function() {
+    // if larger viewport and multi-row layout needed
+    if (breakpoint.matches === true) {
+        // clean up old instances and inline styles when available
+        if (swiperHero !== undefined) swiperHero.destroy(true, true);
+        // or/and do nothing
+        return;
+        // else if a small viewport and single column layout needed
+    } else if (breakpoint.matches === false) {
+        // fire small viewport version of swiper
+        return enableSwiper();
+    }
+};
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+const enableSwiper = function() {
+    swiperHero = new Swiper('.hero__slider', {
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function(i, className) {
+                return `
+   <button class="${className}">
+  <svg class="progress" width="41" height="41"><circle class="circle-origin" r="20" cx="20.5" cy="20.5"></circle></svg><span>${i + 1
+        }</span>
+  </button>
+      `;
+            }
+        },
+        autoHeight: true,
+        spaceBetween: 30
+    });
+};
+
+
+const sliderThumbs = new Swiper('.single-product__thumbs .swiper-container', { // ищем слайдер превью по селектору
+    // задаем параметры
+    direction: 'vertical', // вертикальная прокрутка
+    slidesPerView: 3, // показывать по 3 превью
+    spaceBetween: 9, // расстояние между слайдами
+    navigation: { // задаем кнопки навигации
+        nextEl: '.single-product__next', // кнопка Next
+        prevEl: '.single-product__prev' // кнопка Prev
+    },
+    freeMode: true, // при перетаскивании превью ведет себя как при скролле
+    breakpoints: { // условия для разных размеров окна браузера
+        0: { // при 0px и выше
+            direction: 'horizontal', // горизонтальная прокрутка
+            slidesPerView: 2,
+        },
+        768: { // при 768px и выше
+            direction: 'vertical', // вертикальная прокрутка
+        }
+    }
+});
+// Инициализация слайдера изображений
+const sliderImages = new Swiper('.single-product__images .swiper-container', { // ищем слайдер превью по селектору
+    // задаем параметры
+    direction: 'vertical', // вертикальная прокрутка
+    slidesPerView: 1, // показывать по 1 изображению
+    spaceBetween: 32, // расстояние между слайдами
+    mousewheel: true, // можно прокручивать изображения колёсиком мыши
+    navigation: { // задаем кнопки навигации
+        nextEl: '.single-product__next', // кнопка Next
+        prevEl: '.single-product__prev' // кнопка Prev
+    },
+    grabCursor: true, // менять иконку курсора
+    thumbs: { // указываем на превью слайдер
+        swiper: sliderThumbs // указываем имя превью слайдера
+    },
+    breakpoints: { // условия для разных размеров окна браузера
+        0: { // при 0px и выше
+            direction: 'horizontal', // горизонтальная прокрутка
+        },
+        768: { // при 768px и выше
+            direction: 'vertical', // вертикальная прокрутка
+        }
+    }
+});
+
+
+
+
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+// keep an eye on viewport size changes
+breakpoint.addListener(breakpointChecker);
+// kickstart
+breakpointChecker();
 
 // Подключение анимаций по скроллу
 // import AOS from 'aos';
@@ -84,3 +234,15 @@ console.log(mobileCheck())
 // };
 
 // validateForms('.form-1', rules1, afterForm);
+import { accordion } from './functions/gianniAccordion.min';
+let faq = new gianniAccordion({
+    elements: ".faq__accordion .faq__item",
+    trigger: "[data-accordion-element-trigger]",
+    content: "[data-accordion-element-content]",
+    openAtLandingIndex: 0,
+});
+let table = new gianniAccordion({
+    elements: ".tabs__accardion .tabs__tpanel",
+    trigger: "[data-accordion-element-trigger]",
+    content: "[data-accordion-element-content]",
+});
